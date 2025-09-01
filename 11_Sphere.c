c1fee29858aa57ecb977f9bb24740eac64346665
JannatMIM25 <mawajannatul704@gmail.com>
2025-09-01 20:33:36 +0600

    Update 11_Sphere.c

--- 11_Sphere.c
+++ 11_Sphere.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    double R,pie,V;
+    pie = 3.14159;
+    scanf("%lf",&R);
+    V =(4/3.0)*pie*R*R*R;
+    printf("VOLUME = %.3f\n",V);
+    return 0;
+
+
+
+
+}
 
