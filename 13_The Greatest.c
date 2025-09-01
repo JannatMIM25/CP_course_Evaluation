fd8afbe7737286b424f754adc7751ebab31c86d9
JannatMIM25 <mawajannatul704@gmail.com>
2025-09-01 20:41:44 +0600

    Update 13_The Greatest.c

--- 13_The Greatest.c
+++ 13_The Greatest.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+int main()
+{
+
+    int arr[3];
+    int max=-1;
+    for(int i=0; i<=2; i=i+1)
+    {
+        scanf("%d",&arr[i]);
+        if(max<arr[i])
+        {
+            max=arr[i];
+        }
+    }
+    printf("%d eh o maior\n",max);
+
+    return 0;
+}
+
+
 
